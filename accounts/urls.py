5e4bc7270e6b6c602278cab37d7ce71f71d5b651
mallick-portfolio <tamal1714019@gmail.com>
2023-12-31 14:09:23 +0600

    added login and registration api

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,12 @@
 
 from django.urls import path
 from accounts import views
+from rest_framework_simplejwt import views as jwt_views
 
 
 urlpatterns = [
     path('register/', views.RegistrationAPIView.as_view(), name='register' ),
+    path('login/', views.LoginAPIView.as_view(), name='login' ),
+    path('token-refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,8 @@ from .serializers import RegistrationSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.contrib.auth import authenticate, login, logout
+from . import helpers
 # Create your views here.
 
 
@@ -25,4 +27,38 @@ class RegistrationAPIView(APIView):
           'error': serializer.errors
         })
     except Exception as e:
-      return Response({'message':'fail','error':e,"status": status.HTTP_400_BAD_REQUEST})
+      return Response({'message':'fail','error':e,"status": status.HTTP_500_INTERNAL_SERVER_ERROR})
+
+
+
+class LoginAPIView(APIView):
+  def post(self, request):
+    try:
+      rd = request.data
+      if 'email' not in rd or 'password' not in rd:
+        return Response({
+          "success": False,
+          "status": status.HTTP_400_BAD_REQUEST,
+          'message': "Invalid Credentials",
+        })
+      email = rd['email']
+      password = rd['password']
+      user = authenticate(request, email=email, password=password)
+      if user is not None:
+        login(request, user=user)
+        token = helpers.get_tokens_for_user(user)
+        return Response({
+          "success": True,
+          "status": status.HTTP_200_OK,
+          'message': "Login successfull!!!",
+          "token": token
+        })
+      else:
+        return Response({
+          "success": False,
+          "status": status.HTTP_401_UNAUTHORIZED,
+          'message': "Invalid Credentials",
+        })
+
+    except Exception as e:
+      return Response({'message':'fail','error':e,"status": status.HTTP_500_INTERNAL_SERVER_ERROR})
