0f163cb43f158c0b06fb2a45def465435e5aabf7
mallick-portfolio <tamal1714019@gmail.com>
2023-12-31 14:59:54 +0600

    added task board models

---
+++ task_board/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from accounts.models import CustomUser
+# Create your models here.
+class Board(models.Model):
+  title = models.CharField(_("board name"), max_length=100)
+  description = models.TextField(_("board description"))
+  creator = models.ForeignKey(CustomUser, on_delete=models.CASCADE, )
+  authorize_users = models.ManyToManyField(CustomUser, related_name='boards')
+
+  # time stapm
+  created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=False, auto_now_add=False)
+
+
+class TaskItem(models.Model):
+  title = models.CharField(_("task item title"), max_length=150)
+  creator = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True, blank=True)
+  board = models.ForeignKey(Board, on_delete=models.CASCADE, null=True, blank=True)
+
+  # time stapm
+  created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=False, auto_now_add=False)
+
+
+class Task(models.Model):
+  title = models.CharField(_("task title"), max_length=150)
+  banner = models.ImageField(_("task banner image"), upload_to='taskboard/', max_length=None)
+  creator = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True, blank=True)
+  task_item = models.ForeignKey(TaskItem, on_delete=models.CASCADE, null=True, blank=True)
+   # time stapm
+  created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=False, auto_now_add=False)
+
+
+
--- task_management/settings.py
+++ task_management/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import datetime
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mdft)j0k)ad8ou=!osuxny1k4xjfykm=x=n+cze81j0nv@)e16'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -40,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     # custom app
-    'accounts'
+    'accounts',
+    'task_board'
 ]
 
 MIDDLEWARE = [
