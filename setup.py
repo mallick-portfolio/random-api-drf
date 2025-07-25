d9cbdc2b4a8a7dd7f1ae09afdfe56afddc0bbc79
mallick-portfolio <tamal1714019@gmail.com>
2025-07-25 22:10:03 +0600

    update script to rename folder

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 import subprocess
 import pathlib
 import shutil
-
+import os
 
 repo_url = "https://github.com/mallick-portfolio/random-api-frontend.git"
+parent_dir = pathlib.Path(__file__).resolve().parent.parent
 
-
-target = pathlib.Path(__file__).resolve().parent.parent / "frontend"
+target = parent_dir / "frontend"
 
 subprocess.run(["git", "clone", repo_url, str(target)], check=True)
 
@@ -17,3 +17,15 @@ target_path = current_path.parent.parent / "docker-compose.yaml"
 # Move the file
 shutil.move(str(current_path), str(target_path))
 
+
+
+# Rename the backend folder
+
+old_folder = parent_dir / "random-api-drf"
+new_folder = parent_dir / "backend"
+
+if old_folder.exists():
+    os.rename(old_folder, new_folder)
+    print(f"✅ Renamed '{old_folder.name}' to '{new_folder.name}'")
+else:
+    print("❌ Folder 'random-api-drf' does not exist.")
